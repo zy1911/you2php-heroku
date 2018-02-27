28db6e48adaf9452f5a35548969de03e20df311f
zy1911 <zyz1911@gmail.com>
2018-02-27 14:53:05 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDgyJSB949BYkOMcY27Vc33uBWhOHIDmvA');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
